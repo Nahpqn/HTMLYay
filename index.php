fa1d209920c2cd03f5c37ed55e2cb37234d68b61
Student <student@Cardosi-iMac.local>
2017-02-10 12:35:47 -0500

    Fixed Ryan's mistake

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Untitled Document</title>
+  <link rel="stylesheet" href="css/style.css" type="text/css">
+  <meta charset="utf-8">
+  <meta name="viewport" content="initial-scale=1, width=device-width">
+</head>
+<body>
+<!--Header-->
+<?php include 'navbar.php'; ?>
+
+
+<script src="script/slideShow.js"></script>
+</body>
+</html>
